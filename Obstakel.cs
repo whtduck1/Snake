5af180008b64d73f9a285bce7ad466c59a7570e5
whtduck1 <myszormateusz02@gmail.com>
2025-12-31 12:58:09 +0100

    ypos, draw, obstakel

--- Obstakel.cs
+++ Obstakel.cs
@@ -1,13 +1,23 @@
 public class Obstakel
-
 {
-
     public int Xpos { get; set; }
-
-    // ?
+    public int Ypos { get; set; }
 
     public ConsoleColor schermKleur { get; set; }
-
     public string karacter { get; set; }
+    public Obstakel(int x, int y, ConsoleColor kolor = ConsoleColor.Red, string znak = "█")
+    {
+        Xpos = x;
+        Ypos = y;
+        schermKleur = kolor;
+        karacter = znak;
+    }
 
+    public void Draw()
+    {
+        Console.ForegroundColor = schermKleur;
+        Console.SetCursorPosition(Xpos, Ypos);
+        Console.Write(karacter);
+        Console.ResetColor();
+    }
 }
